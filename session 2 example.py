dce87e8fca1cc1b1409570e6bdadf4a78ba9c917
Pratuat Amatya <pratuat@gmail.com>
2022-10-29 21:01:37 +0200

    list and dictinaries

---
+++ session 2 example.py
@@ -0,0 +1,74 @@
+from ctypes.wintypes import WORD
+
+
+no_of_contries = 200    # integer
+height = 1.75           # float (decimal)
+
+name = "Alex Malla"
+fruit = "Banana"
+is_active = True
+
+
+
+
+# array: list of data (list of any set of data), data could be numbers or strings or boolean
+
+# Python list
+shopping_list = [
+    "Banana",
+    "Milk",
+    "Juice",
+    "Pasta",
+    "Ice-cream"
+]
+
+# Real world list
+Key/Index         Value
+0           "Banana"
+1           "Milk"
+2           "Juice"
+3           "Pasta"
+4           "Ice-cream"
+
+
+lottery_numbers = [34, 56, 78, 90]
+alex_information = ["Alex Malla", 16, 15, True]
+
+
+# dict: dictinary
+
+# python dictionary
+my_dictionary = {
+    "elucidate" : "make (something) clear; explain.",
+    "lol" : "Laught Out Loud",
+    "yolo" : "You Only Live Once",
+    "ruminate" : "think deeply about something",
+}
+
+
+# real world dictionary
+Key             Value
+"elucidate"     "make (something) clear; explain."
+"lol"           "Laught Out Loud"
+"ruminate"      "think deeply about something"
+"yolo"          "You Only Live Once"
+
+# key in left, value in right
+
+epl_captains = {
+    "tottenham" : "Lorris",
+    "chelsea" : "Ceaser",
+    "manchester_city" : "Gundogan",
+    "manchester_united" : "Harry",
+}
+
+country_capitals = {
+    "Norway" : "Oslo",
+    "Denmark" : "Copenhagen",
+    "Germany" : "Berlin",
+    "Finland" : "Helsinki",
+    "Nepal" : "Kathmandu",
+    "India" : "Delhi",
+}
+
+# Think of few examples on dictinary.
