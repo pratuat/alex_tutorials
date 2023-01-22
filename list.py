506c54f41a62a8e0e3bc0b91f2e33341d9d63614
Pratuat Amatya <pratuat@gmail.com>
2023-01-22 20:38:36 +0100

    list

---
+++ list.py
@@ -0,0 +1,29 @@
+# List represents collection of data in ordinal manner.
+# List is represented in python by a [] (square bracket).
+# Examples:
+
+# list of integer numbers
+list_of_prime_numbers = [1, 2, 3, 5, 7, 11, 13, 17, 23, 37]
+list_of_event_numbers = [2, 4, 6, 8, 10]
+
+# list of decimal numbers (floating numbers)
+list_of_scores = [11.2, 45.6, 56.0, 0.67]
+
+# list of strings
+list_of_countries = ["Norway", "Sweden", "Denmark", "Finland"]
+# Index               0         1        2           3
+
+
+# list of mixed data types
+my_information = ["Alex", "Malla", 15, 11, 1.65, "Norewgian", "Asker"]
+# Index            0      1       2   3   4      5            6
+
+# Accessing list by index
+my_information[1]
+my_information[2]
+
+# Updating list
+my_information[0] = "Alexander"
+
+# Accessing all elements in a list
+
